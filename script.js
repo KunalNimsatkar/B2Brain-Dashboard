e9742bcce5c163d931171e512c8f48d29e8a65db
Kunal <kunal.nimsatkar@gmail.com>
2022-09-17 10:17:05 +0530

    dashboard menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const AccountArrow = document.getElementById('account_arrow');
+const PreferenceArrow = document.getElementById('preferences_arrow');
+const Dropdown = document.querySelector('.dropdown');
+
+
+
+AccountArrow.addEventListener('click',DropdownCollapse);
